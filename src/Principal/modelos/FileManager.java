816a55694addd0935c467526dffa30bc260cb28f
ojeanfeli_pe <jean.felipe.moreira12@gmail.com>
2024-05-08 09:03:27 -0300

    Adicionando comentários no FileManager

--- src/Principal/modelos/FileManager.java
+++ src/Principal/modelos/FileManager.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileManager {
-    private String bibliotecaTx;
+    private String bibliotecaTx; // Caminho do arquivo de texto
 
     public FileManager(String bibliotecaTx) {
-        this.bibliotecaTx = bibliotecaTx;
+        this.bibliotecaTx = bibliotecaTx; // Inicializa o caminho do arquivo
     }
 
     // Método para salvar dados em arquivo de texto
     public void salvarDados(List<String> dados){
-        try (PrintWriter writer = new PrintWriter(new FileWriter(bibliotecaTx))){
+        try (PrintWriter writer = new PrintWriter(new FileWriter(bibliotecaTx))){ // Tenta abrir o arquivo para escrita
             for(String linha : dados){
-                writer.println(linha);
+                writer.println(linha); // Escreve cada linha dos dados no arquivo
             }
             System.out.println("Dados salvos com sucesso!");
         }catch (IOException e){
@@ -24,16 +24,16 @@ public class FileManager {
     }
     // Método para carregar dados de arquivo de texto
     public List<String> carregarDados(){
-        List<String> dados = new ArrayList<>();
-        try (FileInputStream fis = new FileInputStream(bibliotecaTx);
+        List<String> dados = new ArrayList<>(); // Lista para armazenar os dados lidos
+        try (FileInputStream fis = new FileInputStream(bibliotecaTx); // Tenta abrir o arquivo para leitura
              BufferedReader reader = new BufferedReader(new InputStreamReader(fis))){
             String linha;
-            while ((linha = reader.readLine()) != null){
-                dados.add(linha);
+            while ((linha = reader.readLine()) != null){ // Lê cada linha do arquivo
+                dados.add(linha);  // Adiciona a linha à lista de dados
             }
-        }catch (IOException e){
+        }catch (IOException e){ // Trata possíveis exceções de IO
             System.out.println("Erro ao carregar os dados: " + e.getMessage());
         }
-        return dados;
+        return dados; // Retorna os dados lidos do arquivo
     }
 }
